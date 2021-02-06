a184f8142a7952bbaf7816161498644a6de6e0a4
mongoz <makarivanov17y@gmail.com>
2021-02-06 13:59:00 +0200

    itstep 1.2

--- lesson4/analyze_number.py
+++ lesson4/analyze_number.py
@@ -1,8 +1,8 @@
 a: int = int(input("Enter the integer number\n\t"))
-if a % 2 == 0 and a >0:
+if a % 2 == 0 and a > 0:
     smile_parity = "Even positive number"
     print(a, "is the", smile_parity)
-elif a % 2 != 0 and a >0:
+elif a % 2 != 0 and a > 0:
     smile_parity = "Odd positive number"
     print(a, "is the", smile_parity)
 elif a % 2 == 0:
