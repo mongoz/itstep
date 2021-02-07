928fff498eba6f201cd3795ab465bfef2db1243b
mongoz <makarivanov17y@gmail.com>
2021-02-07 16:45:36 +0200

    itstep 1.2

---
+++ lesson5/diary.py
@@ -0,0 +1,20 @@
+# day
+# =
+# ["Sunday","Monday", "Tuesday", "Wednesday",
+# "Thursday",
+# "Friday",
+# "Saturday"]
+
+day = int(input("Enter the day \n"))
+task = [
+
+    "Monday go for a walk; read the book",
+    "Tuesday meet friend Andrew at 5 p.m; go to the gym",
+    "Wednesday do homework; play 'UFC:3'",
+    "Thursday go for a walk; read the book",
+    "Friday meet friend Andrew; at 5 p.m go to the gym",
+    "Saturday do homework; play 'UFC:3'",
+    "Sunday go for a walk; read the book"
+]
+if day in range(0, 8):
+    print(f"{task[day]}")
