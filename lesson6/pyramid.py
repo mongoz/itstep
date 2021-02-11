6bc02b93604041cdd37a20db1430db9deaa44f30
mongoz <makarivanov17y@gmail.com>
2021-02-11 22:52:16 +0200

    itstep 1.2

--- lesson6/pyramid.py
+++ lesson6/pyramid.py
@@ -3,5 +3,5 @@ for i in range(0,num):
     for j in range(0,num-i-1):
         print(end=" ")
     for j in range(0,i+1):
-        print("*",end=" ")
+        print("*", end=" ")
     print()
---
+++ lesson6/simpler_loop.py
@@ -0,0 +1,2 @@
+for x in range(0, 31, 5):
+    print(x)
