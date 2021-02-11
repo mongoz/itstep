d4d4e1ff43d99df584ae989bdff7b9030047d62c
mongoz <makarivanov17y@gmail.com>
2021-02-11 23:41:46 +0200

    itstep 1.2

--- lesson6/user_pyramid.py
+++ lesson6/user_pyramid.py
@@ -1,7 +1,7 @@
 num = int(input("Enter the number of rows:\n"))
-for i in range(0, num):
-    for j in range(0, num-i-1):
+for x in range(0, num):
+    for y in range(0, num - x - 1):
         print(end=" ")
-    for j in range(0, i+1):
+    for y in range(0, x + 1):
         print("*", end=" ")
     print()
