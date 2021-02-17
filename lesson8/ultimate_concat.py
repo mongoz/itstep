be87a0b9a4947ceba01ebfc9cfe9a8bbbe88f6cc
mongoz <makarivanov17y@gmail.com>
2021-02-17 17:10:27 +0200

    itstep 1.2

--- lesson8/ultimate_concat.py
+++ lesson8/ultimate_concat.py
@@ -1,4 +1,9 @@
 first_list = ['Neo', 'Trinity', 'Mouse']
 second_list = ['Matrix', 'Forever', 'Alone']
-output_list = [f'{m} {n}' for m in first_list for n in second_list]
-print(output_list)
+output_list =[]
+if len(second_list) != len(first_list):
+    print('list length mismatch')
+else:
+    for each in range(0, len(second_list)):
+        output_list.append(first_list[each] + str(second_list[each]))
+
