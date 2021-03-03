2f6444638eea5b09e497773ec9cb7727084ea788
mongoz <makarivanov17y@gmail.com>
2021-03-03 21:06:12 +0200

    itstep 1.2

--- exam1/text.py
+++ exam1/text.py
@@ -2,10 +2,5 @@
 # print(f"{text[0:4]}")
 
 text = "The core of extensible programming is defining functions."
-for letter in enumerate(text, 1):
-    print(text[0])
-    print(text[1])
-    print(text[2])
-    print(text[3])
-
-#
+for letter in text[0:3]:
+    print(letter[0:3])
