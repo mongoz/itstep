e58912b479ae5df57c40440fd71e93d1720f3f74
Manager-1 <makarivanov17y@gmail.com>
2021-04-30 08:37:52 +0300

    itstep1.2

--- lesson20/deck.py
+++ lesson20/deck.py
@@ -1,17 +1,19 @@
 import random
 
 
+def shuffle(cards):
+    random.shuffle(cards)
+    print(f"Перетасованая колода: \n{cards}")
+
+
+def take_cards(card_del, card):
+    card.remove(card_del)
+    return card
+
+
 class Deck:
     def __init__(self, cards):
         self._card = cards
-
-    def shuffle(self, cards):
-        random.shuffle(cards)
-        print(f"Перетасованая колода: \n{cards}")
-
-    def take_cards(self, card_del, card):
-        card.remove(card_del)
-        return card
 
     def __str__(self):
         return f"Новая колода: \n{self._card}"
@@ -20,5 +22,5 @@ class Deck:
 card_example = [('A', 'Spades'), ('K', 'Clubs'), (10, 'Diamonds'),
                 (5, 'Hearts')]
 deck = Deck(card_example)
-deck.shuffle(card_example)
-print(f"Новая колода: {deck.take_cards(('A', 'Spades'), card_example)}")
+shuffle(card_example)
+print(f"Новая колода: {take_cards(('A', 'Spades'), card_example)}")
