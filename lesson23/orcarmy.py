2ddcb9871c8741381ed1bac02ebeddf76437792e
Manager-1 <makarivanov17y@gmail.com>
2021-05-13 18:11:44 +0300

    'itstep 1.2'

--- lesson23/orcarmy.py
+++ lesson23/orcarmy.py
@@ -1,43 +1,47 @@
 class Army:
     def __init__(self, warrior_amount, damage_per_warrior, warrior_health):
         self.warrior_amount = warrior_amount
-        self.damage = damage
-        self.health = health
+        self.damage_per_warrior = damage_per_warrior
+        self.warrior_health = warrior_health
 
     def __str__(self):
-        return f"{self.amount}, {self.damage}, {self.health}"
+        return f"{self.warrior_amount}, {self.damage_per_warrior}, {self.warrior_health}"
 
-    def __add__(self, other):
-        return Army(self.amount + other.amount,
-                       (self.damage*self.amount + other.damage*other.amount)/(self.amount + other.amount),
-                       (self.health*self.amount + other.health*other.amount)/(self.amount + other.amount))
+    def __add__(self, another_parm):
+        return Army(self.warrior_amount + another_parm.amount,
+                    (self.damage_per_warrior * self.warrior_amount + another_parm.damage_per_warrior *
+                     another_parm.warrior_amount) / (
+                                self.warrior_amount + another_parm.amount),
+                    (self.warrior_health * self.warrior_amount + another_parm.warrior_health *
+                     another_parm.warrior_amount) / (
+                                self.warrior_amount + another_parm.amount))
 
-    def __sub__(self, other):
-        if self.amount - other.amount > 0:
-            return Army(self.amount - other.amount, self.damage, self.health)
+    def __sub__(self, another_parm):
+        if self.warrior_amount - another_parm.amount > 0:
+            return Army(self.warrior_amount - another_parm.amount, self.damage_per_warrior, self.warrior_health)
         else:
-            return Army(0, self.damage, self.health)
+            return Army(0, self.damage_per_warrior, self.warrior_health)
 
 
 class OrkArmy(Army):
-    def receive_damage(self, damage: int):
-        died_orks = (self.amount * self.health / damage)
-        if self.amount > died_orks:
-            self.amount = self.amount - died_orks
-            return (died_orks)
+    def receive_damage(self, damage_per_warrior: int):
+        rip_ork = (self.warrior_amount * self.warrior_health / damage_per_warrior)
+        if self.warrior_amount > rip_ork:
+            self.warrior_amount = self.warrior_amount - rip_ork
+            return rip_ork
         else:
-            return (self.amount)
+            return self.warrior_amount
 
 
 class ElfArmy(Army):
-    def __init__(self, amount, damage, health, shield):
-        Army.__init__(self, amount, damage, health)
+    def __init__(self, warrior_amount, damage_per_warrior, warrior_health, shield):
+        Army.__init__(self, warrior_amount, damage_per_warrior, warrior_health)
         self.shield = shield
 
     def receive_damage(self, damage: int):
-        died_elfs = ((self.amount * (self.health + self.shield)) / damage)
-        if self.amount > died_elfs:
-            self.amount = self.amount - died_elfs
-            return (died_elfs)
+        rip_elf = ((self.warrior_amount * (self.warrior_health + self.shield)) / damage)
+        if self.warrior_amount > rip_elf:
+            self.warrior_amount = self.warrior_amount - rip_elf
+            return rip_elf
         else:
-            return (self.amount)
+            return self.warrior_amount
